ced9914e25c7fa493ddee489964e6fdc903250b2
Kushak1 <42020387+Kushak1@users.noreply.github.com>
2020-06-09 22:12:14 +0400

    Create MarkAndToys.js

---
+++ MarkAndToys.js
@@ -0,0 +1,14 @@
+    function maximumToys(prices, k) {
+        
+    let count = 0;
+
+    let sorted = prices.sort( (a,b) => {return a - b} );
+
+    for(let i = 0;i<sorted.length;i++) {
+        if(k - sorted[i] >= 0) {
+            k -= sorted[i];
+            count++;
+        }
+    }
+    return count;
+}
